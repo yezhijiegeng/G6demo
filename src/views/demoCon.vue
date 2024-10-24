033e196b04a7f4dd97dbec622c6a9df8e3e27dc6
Du Qing <duqingBetter@qq.com>
2024-10-25 01:21:30 +0800

    已修改        babel.config.js 已修改        package-lock.json 已修改        package.json 已修改        src/views/demo3.vue 缺失         src/views/demo4.vue 已修改        src/views/demoCon.vue

--- src/views/demoCon.vue
+++ src/views/demoCon.vue
@@ -1,57 +1,59 @@
 <template>
   <el-container class="h-full">
-  <!-- <el-header>Header</el-header> -->
-  <el-container>
-    <el-aside style="border-right: 1px solid #ccc; width: 200px;">
-      <el-menu
-        :default-active="activeName"
-        @select="handleSelect"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose">
-        <el-menu-item index="1">
-          <i class="el-icon-document"></i>
-          <span slot="title">demo1</span>
-        </el-menu-item>
-        <el-menu-item index="2">
-          <i class="el-icon-document"></i>
-          <span slot="title">demo2</span>
-        </el-menu-item>
-      </el-menu>
-    </el-aside>
-    <el-main>
-      <demo1 class="h-full w-full" v-if="activeName==='1'" />
-      <demo2 class="h-full w-full" v-if="activeName==='2'" />
-    </el-main>
+    <!-- <el-header>Header</el-header> -->
+    <el-container>
+      <el-aside style="border-right: 1px solid #ccc; width: 200px;">
+        <el-menu :default-active="activeName" @select="handleSelect" class="el-menu-vertical-demo" @open="handleOpen"
+          @close="handleClose">
+          <el-menu-item index="1">
+            <i class="el-icon-document"></i>
+            <span slot="title">demo1</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <i class="el-icon-document"></i>
+            <span slot="title">demo2</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-document"></i>
+            <span slot="title">demo3</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <demo1 class="h-full w-full" v-if="activeName === '1'" />
+        <demo2 class="h-full w-full" v-if="activeName === '2'" />
+        <demo3 class="h-full w-full" v-if="activeName === '3'" />
+      </el-main>
+    </el-container>
   </el-container>
-</el-container>
 </template>
 
 <script>
 import demo1 from './demo1.vue'
 import demo2 from './demo2.vue'
+import demo3 from './demo3.vue'
 export default {
   name: 'HomeView',
   components: {
     demo1,
     demo2,
+    demo3,
   },
   data() {
     return {
-      activeName: '2',
+      activeName: '3',
     }
   },
   methods: {
-    handleSelect(val){
+    handleSelect(val) {
       this.activeName = val
     },
-    handleOpen(){},
-    handleClose(){},
+    handleOpen() { },
+    handleClose() { },
   },
-  mounted(){},
+  mounted() { },
 }
 </script>
 <style lang="scss" scoped>
-.demoCon {
-}
+.demoCon {}
 </style>
